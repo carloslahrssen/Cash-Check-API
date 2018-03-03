200e942105bb3cafa280d64c9e6d080ffa944195
carloslahrssen <carloslahrssen@gmail.com>
2018-03-03 15:39:22 -0500

    Plaid api fully implemented

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const express = require('express');
 const app = express();
 // const router = app.Router();
 const mongoose = require('mongoose');
-const plaid = require('plaid');
 const config = require('./config');
 const bodyParser = require('body-parser');
 //Initial Connection to the database
 mongoose.connect('mongodb://fsuhacks_database_1/plaid');
-//Initializing plaid API
-const plaidClient = new plaid.Client(config.CLIENT_ID, config.SECRET, config.PUBLIC_KEY, plaid.environments[config.PLAID_ENV]);
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
